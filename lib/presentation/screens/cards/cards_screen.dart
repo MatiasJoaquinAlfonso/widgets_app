9bf2c367670bced9f5be20007976e254304d88b7
Alfonso Matias Joaquin <malfonso679@alumno.unlam.edu.ar>
2025-03-04 14:11:33 -0300

    Rutas basicas configuradas

---
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CardsScren extends StatelessWidget {
+  const CardsScren({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards Screen'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
