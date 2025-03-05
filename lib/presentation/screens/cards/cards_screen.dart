5cdecf22c9ca98d6b0b027d5a57f215f7b9aa6e1
Alfonso Matias Joaquin <malfonso679@alumno.unlam.edu.ar>
2025-03-05 19:08:51 -0300

    rutas con nombre implementado

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CardsScren extends StatelessWidget {
+  
+  static const String name = 'cards_screen';
+
   const CardsScren({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cards Screen'),
-      ),
+      appBar: AppBar(title: Text('Cards Screen')),
       body: const Placeholder(),
     );
   }
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const name = 'home_screen';
+
   const HomeScreen({super.key});
 
   @override
@@ -53,7 +56,8 @@ class _CustomListTitle extends StatelessWidget {
         // );
 
         // Navigator.pushNamed(context, menuItem.link);
-        context.push(menuItem.link);
+        // context.push(menuItem.link);
+        context.pushNamed( CardsScren.name );
       },
     );
   }
