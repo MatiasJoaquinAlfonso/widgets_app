2cfe2016ed55126f3f2189779b042d83cb7dbfd5
Alfonso Matias Joaquin <malfonso679@alumno.unlam.edu.ar>
2025-03-18 00:41:27 -0300

    riverpod y pantalla para cambiar colores

---
+++ lib/presentation/providers/theme_provider.dart
@@ -0,0 +1,8 @@
+//Estado => isDarkmodeProvider = boolean
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+
+final isDarkmodeProvider = StateProvider((ref) => false);
+
+// Listado de colores inmutables (nunca se puede cambiar)
+final colorListProvider = Provider((ref) => colorList);
--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   static const String name = 'counter_screen';
-  final int count = 0; 
+
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkmodeProvider);
+
     return Scaffold(
-      appBar: AppBar(title: Text('Contador')),
+      appBar: AppBar(
+        title: Text('Contador'),
+        actions: [
+          IconButton(
+            icon: Icon( isDarkMode ? Icons.dark_mode_outlined : Icons.light_mode_outlined),
+            onPressed: () {
+              ref.read(isDarkmodeProvider.notifier).update((isDarkMode) => !isDarkMode);
+            },
+          ),
+        ],
+      ),
       body: Center(
-        child: Text('Valor: $count', style: Theme.of(context).textTheme.titleLarge,),
+        child: Text(
+          'Valor: $clickCounter ',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
       ),
 
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: (){
-
-        }
+        onPressed: () {
+          //ref.read(counterProvider.notifier).update((state) => state + 1);
+          ref.read(counterProvider.notifier).state++;
+        },
       ),
     );
   }
